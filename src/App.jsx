28b4543abdc79d6b5fceeac6437ed80dbd8c5dc7
HuzaiFa A <huziafarazaa999@gmail.com>
2026-03-03 00:58:01 +0500

    Game Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,118 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import { Fragment } from "react/jsx-runtime";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Circle({ value, circleClick }) {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="circle" onClick={circleClick}>
+      {value}
+    </div>
+  );
 }
 
-export default App
+export default function App() {
+  const [circles, setCircles] = useState(Array(9).fill(null));
+  const [xNext, setXnext] = useState(false);
+  const guideRef = useRef(null);
+
+  function declareWinner(circles) {
+    const winnerConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let index = 0; index < winnerConditions.length; index++) {
+      const [a, b, c] = winnerConditions[index];
+      if (
+        circles[a] &&
+        circles[a] === circles[b] &&
+        circles[a] === circles[c]
+      ) {
+        return circles[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = declareWinner(circles);
+  let status;
+  if (winner) {
+    status = `${winner} Win`;
+  } else {
+    status = !xNext ? "X Turn" : "O Turn";
+  }
+
+  function circleClickHandler(index) {
+    if (circles[index] || winner) {
+      guideRef.current.textContent = `${declareWinner(circles)} Win`;
+      return;
+    }
+    const circlesHistory = circles.slice();
+    if (!xNext) {
+      circlesHistory[index] = "X";
+    } else {
+      circlesHistory[index] = "O";
+    }
+    setCircles(circlesHistory);
+    setXnext(!xNext);
+  }
+
+  const firstRow = [0, 1, 2];
+  const secondRow = [3, 4, 5];
+  const thirdRow = [6, 7, 8];
+
+  function againStartHandler() {
+    setCircles(Array(9).fill(null));
+  }
+
+  return (
+    <Fragment>
+      <main>
+        <div className="board">
+          <div className="row">
+            {firstRow.map((element, index) => {
+              return (
+                <Circle
+                  key={index}
+                  value={circles[element]}
+                  circleClick={() => circleClickHandler([element])}
+                />
+              );
+            })}
+          </div>
+          <div className="row">
+            {secondRow.map((element, index) => {
+              return (
+                <Circle
+                  key={index}
+                  value={circles[element]}
+                  circleClick={() => circleClickHandler([element])}
+                />
+              );
+            })}
+          </div>
+          <div className="row">
+            {thirdRow.map((element, index) => {
+              return (
+                <Circle
+                  key={index}
+                  value={circles[element]}
+                  circleClick={() => circleClickHandler([element])}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <p className="guide" ref={guideRef}>
+          {status}
+          {winner && <button onClick={againStartHandler}>Again Start</button>}
+        </p>
+      </main>
+    </Fragment>
+  );
+}
